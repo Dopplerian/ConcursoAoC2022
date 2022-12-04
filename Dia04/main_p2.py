8cbc3839107a1feb751720f96cecc6e2e003821a
Dopplerian <dopplerian+git@proton.me>
2022-12-04 13:36:30 +0100

    Dia 4

---
+++ Dia04/main_p2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+contador = 0
+
+for linea in [l.rstrip().split(',') for l in open("input.txt","r").readlines()]:
+	elfo1 = [int(n) for n in linea[0].split('-')]
+	elfo2 = [int(n) for n in linea[1].split('-')]
+	uno_contiene_dos = elfo1[1] >= elfo2[0] and elfo1[1] <= elfo2[1]
+	dos_contiene_uno = elfo2[1] >= elfo1[0] and elfo2[1] <= elfo1[1]
+	if uno_contiene_dos or dos_contiene_uno:
+		contador += 1
+
+print(contador)
